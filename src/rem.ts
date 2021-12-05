d0f59c2d9554c9659dd4174ae65ad4b4d9d05afd
xutong8 <2451836009@qq.com>
2021-12-05 20:01:06 +0800

    fix: 响应式

--- src/rem.ts
+++ src/rem.ts
@@ -3,7 +3,7 @@ const baseSize = 16;
 
 function setRem() {
   // 实际设备页面宽度和设计稿的比值
-  const scale = document.documentElement.clientWidth / 1920;
+  const scale = document.documentElement.clientWidth / 7680;
   // 计算实际的rem值并赋予给html的font-size
   document.documentElement.style.fontSize = baseSize * scale + 'px';
 }
